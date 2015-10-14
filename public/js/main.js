fe0e2e119b80144e3e3a08b16ef72c2450b10321
Ben Roth <spongebucket@gmail.com>
2015-10-14 11:11:47 -0700

    make the nav update correctly as page scrolls

--- public/js/main.js
+++ public/js/main.js
@@ -3,9 +3,6 @@ $(function() {
 	var $targetName = $(this).attr('href').slice(1);
 	var $target = $('#contents').find('div[id=' + $targetName + ']');
 
-	$('#nav a').removeClass('current');
-	$(this).addClass('current');
-
 	var $targetOffset = $target.offset().top;
 	$('html,body')
 	    .animate({
@@ -13,4 +10,15 @@ $(function() {
 	    }, 200, "easeOutQuad", function(){});
 	e.preventDefault();
     });
+
+    $(window).scroll(function() {
+	var windowCenter = $(window).scrollTop() + $(window).height() * 0.5;
+	$('#contents div.full').each(function(ii) {
+	    if ($(this).position().top <= windowCenter) {
+		$('#nav a').removeClass('current');
+		$('#nav a').eq(ii).addClass('current');
+	    }
+	});
+
+    }).scroll();
 });
