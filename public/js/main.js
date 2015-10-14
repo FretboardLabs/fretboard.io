843af4dcce914a9c73ee7618a992ca32f4f622a3
Ben Roth <spongebucket@gmail.com>
2015-10-14 11:00:19 -0700

    make nav current based on clicked link

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,9 @@ $(function() {
 	var $targetName = $(this).attr('href').slice(1);
 	var $target = $('#contents').find('div[id=' + $targetName + ']');
 
+	$('#nav a').removeClass('current');
+	$(this).addClass('current');
+
 	var $targetOffset = $target.offset().top;
 	$('html,body')
 	    .animate({
